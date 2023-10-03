0e78b15e1ee96f10486a7869a6deb4abec9433a0
hyesungpark <jayhspark@gmail.com>
2023-10-04 05:27:10 +0900

    add footer

---
+++ Touchdown/App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Hye Sung Park on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  // MARK: - PROPERTIES
+  
+  // MARK: - BODY
+  var body: some View {
+    FooterView()
+      .padding(.horizontal)
+  }
+}
+
+#Preview {
+  ContentView()
+}
