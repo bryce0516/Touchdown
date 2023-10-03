686d2fd8c001e85396ff32b5d3da1997b077020d
hyesungpark <jayhspark@gmail.com>
2023-10-04 05:19:11 +0900

    add constant

---
+++ Touchdown/View/Home/FooterView.swift
@@ -0,0 +1,18 @@
+//
+//  FooterView.swift
+//  Touchdown
+//
+//  Created by Hye Sung Park on 10/4/23.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FooterView()
+}
