82b5be00fecef0a2a41810ba41f468120c1e5e82
hyesungpark <jayhspark@gmail.com>
2023-10-04 05:01:55 +0900

    Initial Commit

---
+++ Touchdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Hye Sung Park on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Hye Sung Park on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
