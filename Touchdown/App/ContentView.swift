205eca109ffeda9d3ffa138202df8bfe78d855e2
hyesungpark <jayhspark@gmail.com>
2023-10-04 21:29:37 +0900

    handle notch

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -9,18 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
   // MARK: - PROPERTIES
-  
+
   // MARK: - BODY
   var body: some View {
-    VStack(spacing: 0) {
-      NavigationBarView()
-      
-      Spacer()
-      
-      FooterView()
-        .padding(.horizontal)
-    }
-    .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+    ZStack {
+      VStack(spacing: 0) {
+        NavigationBarView()
+          .padding(.horizontal, 15)
+          .padding(.bottom)
+          .padding(.top,
+                   UIApplication.shared.windows.first?.safeAreaInsets.top)
+          .background(Color.white)
+          .shadow(
+            color: Color.black.opacity(0.05), 
+            radius: 5,
+            x: 0,
+            y: 5
+          )
+
+        Spacer()
+        
+        FooterView()
+          .padding(.horizontal)
+      }
+      .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+    } //: ZSTACK
+    .ignoresSafeArea(.all, edges: .top)
   }
 }
 
